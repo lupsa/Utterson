73deaae98b1731f694c306ad5d97de7f01f71f28
Darius Lupșa <dev@dariuslupsa.ro>
2023-03-07 21:41:08 +0200

    This theme is inspired from bearblog.dev with code from https://github.com/poole/poole

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lupsa/Utterson
 
 go 1.17
+
+require github.com/philipbelesky/inter-ui v3.19.0+incompatible
--- layouts/partials/staticman-js.js
+++ layouts/partials/staticman-js.js
@@ -1,50 +1,36 @@
 if (/comment-thankyou/.test(window.location.href)) {
-    document.getElementById('comment-thankyou').style.display = 'block';
+    document.getElementById("comment-thankyou").style.display = "block";
 }else{
-    document.getElementById('comment-thankyou').style.display = 'none';
+    document.getElementById("comment-thankyou").style.display = "none";
 }
 
 function replyTo(parent, name, commentID) {
   var e = document.getElementById(parent),
-      z = document.getElementById('comment-' + commentID),
-      f = document.getElementById('comment-form'),
-      h = document.getElementById('comment-form-header');
+      z = document.getElementById("comment-" + commentID),
+      f = document.getElementById("comment-form"),
+      h = document.getElementById("comment-form-header");
 
-  h.innerHTML = 'Reply to ' + name;
+  h.innerHTML = "Reply to " + name;
   z.parentNode.insertBefore(f, z.nextSibling);
-  document.getElementsByName('fields[reply_to]')[0].value=parent;
+  document.getElementsByName("fields[reply_to]")[0].value=parent;
   document.getElementById("resetButton").hidden = false;
 }
 
-function reset(parent) {
-  var a = document.getElementById('comment-thankyou'),
-      f = document.getElementById('comment-form'),
-      h = document.getElementById('comment-form-header');
+function cancelReply(parent) {
+  var a = document.getElementById("comment-thankyou"),
+      f = document.getElementById("comment-form"),
+      h = document.getElementById("comment-form-header");
 
-  h.innerHTML = 'Your thoughts?';
-  a.parentNode.insertBefore(f, a.previousSibling);
-  document.getElementsByName('fields[reply_to]')[0].value=parent;
+  h.innerHTML = "Your thoughts?";
+  a.parentNode.insertBefore(f, a);
+  document.getElementsByName("fields[reply_to]")[0].value=parent;
   document.getElementById("resetButton").hidden = true;
 }
 
 function checkForm(form){
-
-    if(form.yourname.value == ""){
-        form.warningComment.style.display = 'block'; 
-        form.warningComment.innerText = "Please type your name";
-        return false;
-    }
-    else if(form.yourcomment.value == ""){
-        form.warningComment.style.display = 'block'; 
-        form.warningComment.innerText = "Please type a comment";
-        return false;
-    }
-    else{
-        form.submitButton.disabled = true;
-        form.warningComment.style.display = 'none'; 
-        form.warningComment.innerText = "";
-        form.submitButton.innerText = "Sending...";
-        return true;
-    }
-    
+    form.submitButton.disabled = true;
+    form.warningComment.style.display = "none"; 
+    form.warningComment.innerText = "";
+    form.submitButton.innerText = "Sending...";
+    return true;
 }
